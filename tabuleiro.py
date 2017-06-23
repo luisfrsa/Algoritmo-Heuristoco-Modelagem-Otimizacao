fb51bc76d52170345b6de9ff55fcca0c7276a9da
Luis Fernando Rocha de Sousa Alves <Luis Fernando Rocha de Sousa Alves>
2017-06-22 21:27:52 -0300

    heuristica 2 e finalizando 3

--- tabuleiro.py
+++ tabuleiro.py
@@ -19,12 +19,30 @@ class Tabuleiro:
 	def calculaH1(self,j,i,valor_comparador):
 		if(self.lista_tabuleiro[j][i] != valor_comparador):
 			self.h = self.h+1
-		
-			
+
+	def calculaH2(self,j,i,valor_anterior):
+		if(self.lista_tabuleiro[j][i] != (valor_anterior+1)):
+			self.h = self.h+1	
+
+	def positivo(self,valor):
+		if(valor < 0):
+			return valor*-1
+		return valor	
+
+	def calculaH3(self,j,i,valor_comparador):
+		if(self.lista_tabuleiro[j][i] != valor_comparador):
+			posicao_esperada = self.lista_tabuleiro[j][i]-1
+			alvo_j = posicao_esperada%4
+			alvo_i = posicao_esperada//4
+			errumar
+			self.h = self.h + self.positivo((alvo_j - j) + (alvo_i - i))	
+
+
 	#aqui será: verificado se tabuleiro resolvido, e calculada heuristica
 	def verificaTabuleiroResolvido(self):
-		heuristica = 1
+		heuristica = 3
 		valor_comparador = 1
+		valor_anterior_h2 = self.lista_tabuleiro[0][0]-1
 		tabuleiro_resolvido = True	
 		for i in range (0,4):
 			for j in range (0,4):
@@ -33,17 +51,20 @@ class Tabuleiro:
 				if(heuristica==1):
 					self.calculaH1(j,i,valor_comparador)
 				elif(heuristica==2):
-					a=0
+					self.calculaH2(j,i,valor_anterior_h2)
+					valor_anterior_h2 = self.lista_tabuleiro[j][i]
 				elif(heuristica==3):
+					self.calculaH3(j,i,valor_comparador)
 					a=0
 				elif(heuristica==4):
 					a=0
 				elif(heuristica==5):
 					a=0
-
 				valor_comparador = valor_comparador+1	
 		#if(tabuleiro_resolvido==True):
 			#self.printTabuleiro()
+		#print(self.h)
+		#self.printTabuleiro()
 		return tabuleiro_resolvido
 
 	def stringTabuleiroIJ(i,j):
--- tests.py
+++ tests.py
@@ -1,7 +1,17 @@
-import time
+def calculaH3(valor,j,i,valor_comparador):
+	h=0
+	if(valor != valor_comparador):
+		posicao_esperada = valor-1
+		alvo_j = posicao_esperada%4
+		alvo_i = posicao_esperada//4
+		h =  positivo((alvo_j - j) + (alvo_i - i))
+		print("Valor: ",valor, "posicao[]= ",j,",",i,"Ind comparador",valor_comparador," COM H ",h)
 
-class Testes:
-
-	def sleep(s):
-		time.sleep(s)
-	sleep = staticmethod(sleep)
+def positivo(valor):
+	if(valor < 0):
+		return valor*-1
+	return valor	
+			
+for i in range (1,17):
+	g = i+1
+	calculaH3(i,0,0,g)
